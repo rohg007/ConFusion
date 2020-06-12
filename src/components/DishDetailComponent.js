05c3b7996f6760deb9c035d097eb3e67855e82a2
gupta <17dcs012@lnmiit.ac.in>
2020-06-13 00:41:44 +0530

    Ristorante Confusion React

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+    function RenderDish({dish}) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    function RenderComments({comments}) {
+        if(comments==null){
+            return(
+                <div></div>
+            )
+        }
+        else{
+            const comment = comments.map((comm) => {
+                return(
+                    <li key={comm.id}>
+                        <p>{comm.comment}</p>
+                        <p>-- {comm.author},
+                        &nbsp;
+                        {new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: '2-digit'
+                            }).format(new Date(comm.date))}
+                        </p>
+                    </li>
+                );
+            });
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comment}
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    const DishDetail = (props) =>{
+        return (
+            <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+            </div>
+        );
+    }
+ 
+
+export default DishDetail;
